42cb458c55640232dce57cce3f213e6ed8b1a57e
Adam <alspdx@gmail.com>
2018-01-19 09:54:14 -0800

    add an array of hard-coded animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -12,5 +12,11 @@ import { Animal } from './animal.model'
 })
 
 export class AppComponent {
-
+  masterAnimalList: Animal[] = [
+    new Animal('Fluffy', 'Grizzly Bear', 12, 'Male', 'Carnivore', ['Scratching back on tree trunks', 'Roaring'], ['Hibertnation being interrupted', 'Being poked'], 'East Bear Sanctuary', 5),
+    new Animal('Candy', 'Polar Bear', 7, 'Female', 'Carnivore', ['Playing in the snow', 'Fishing through ice'], ['Global Warming', 'The Desert'], 'Ice Caves', 6),
+    new Animal('Prince Edward', 'Lion', 22, 'Male', 'Carnivore', ['The thrill of the hunt', 'Roaring'], ['Vegetables', 'Not being respected at the watering hole'], 'The Safari Room', 9),
+    new Animal('Dudley', 'Elephant', 48, 'Male', 'Herbivore', ['Mud pits', 'Stamping around'], ['The Circus', 'Mice'], 'The Safari Room', 3),
+    new Animal('Christina', 'Flamingo', 18, 'Female', 'Omnivore', ['Wading', 'Putting her foot down', 'Eating shrimp'], ['Open relationships', 'Standing on two legs'], 'Shallow Shores', 1),
+  ]
 }
